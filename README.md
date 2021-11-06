a336d7934083d1083126e6bfc93c8a053c64968b
Beat Reichenbach <info@artofbeat.com>
2021-11-06 13:43:56 -0700

    Add override to go around scraping.

--- README.md
+++ README.md
@@ -9,5 +9,7 @@ https://wordpress.org/support/article/managing-plugins/
 
 Make sure your personal artstation website is set to the default theme. The plugin is scraping https://\<username\>.artstation.com/pages/portfolio for the first element with class "album-grid".
 
+Unfortunately Cloudflare is detecting requests with Curl and file_get_contents. To get around this, the plugin now allows to override the scraping input by loading an html file manually. Go to https://\<username\>.artstation.com/pages/portfolio and save the source code in a file named _portfolio.html_ in the base directory next to _index.php_.
+
 ## Wordpress Blocks
 https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/writing-your-first-block-type/
--- index.php
+++ index.php
@@ -28,15 +28,29 @@ if ( ! function_exists( 'load_block_attributes' ) ) {
 }
 
 function artstation_gallery_render_block( $block_attributes, $content ) {
+    // page scrapping gets detected by cloudflare, so we manually feed an html site.
+    $contents_file = plugin_dir_path( __FILE__ ) . 'portfolio.html';
+    if ( file_exists( $contents_file ) ) {
+        $url = $contents_file;
+    }
+    else {
+        $username = $block_attributes['username'];
+        $url = sprintf( 'https://%s.artstation.com/pages/portfolio', $username );
+    }
+    // pretend to be a user, otherwise request returns HTTP/1.1 403
+    $context = stream_context_create(
+        array(
+            "http" => array(
+                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36\n" . "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n"
+            )
+        )
+    );
 
-	$username = $block_attributes['username'];
-	$url = sprintf( 'https://%s.artstation.com/pages/portfolio', $username );
-
-	// load contents from artstation portfolio page
-	$contents = file_get_contents( $url );
+    // load contents from artstation portfolio page
+    $contents = @file_get_contents( $url, false, $context);
 
     if ( false === $contents ) {
-    	return null;
+        return null;
     }
 
     libxml_use_internal_errors( true );
@@ -71,50 +85,51 @@ function artstation_gallery_render_block( $block_attributes, $content ) {
     foreach($scripts as $script) {
         $src = $script->getAttribute( 'src' );
         if(strpos($src, 'cloudflare') !== false){
-        	$script->setAttribute( 'id', 'cloudflare' );
+            $script->setAttribute( 'id', 'cloudflare' );
         }
     }
 
     $blockProps = load_block_attributes( $block_attributes );
 
-   	$html = sprintf( '<div %s>%s</div>', $blockProps, $doc->saveHTML( $album ) );
+    $html = sprintf( '<div %s>%s</div>', $blockProps, $doc->saveHTML( $album ) );
     return $html;
 
 }
 
 function artstation_gallery_register_block() {
 
-	if ( ! function_exists( 'register_block_type' ) ) {
-		return;
-	}
+    if ( ! function_exists( 'register_block_type' ) ) {
+        return;
+    }
 
     // automatically load dependencies and version
     $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
 
-	wp_register_script(
-		'artstation-gallery',
-		plugins_url( '/build/index.js', __FILE__ ),
+    wp_register_script(
+        'artstation-gallery',
+        plugins_url( '/build/index.js', __FILE__ ),
         $asset_file['dependencies'],
         $asset_file['version']
-	);
+    );
 
-	wp_register_style(
-		'artstation-gallery',
-		plugins_url( 'style.css', __FILE__ ),
-		array( ),
-		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
-	);
+    wp_register_style(
+        'artstation-gallery',
+        plugins_url( 'style.css', __FILE__ ),
+        array( ),
+        filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+    );
 
-	register_block_type( 'artstation/gallery', array(
-		'api_version' => 2,
-		'style' => 'artstation-gallery',
-		'editor_script' => 'artstation-gallery',
-		'render_callback' => 'artstation_gallery_render_block',
-		'attributes' => array(
-			'username' => array(
-				'type' => 'string',
-				'default' => ''
-				),
+    register_block_type( 'artstation/gallery', array(
+        'api_version' => 2,
+        'style' => 'artstation-gallery',
+        'editor_style' => 'artstation-gallery',
+        'editor_script' => 'artstation-gallery',
+        'render_callback' => 'artstation_gallery_render_block',
+        'attributes' => array(
+            'username' => array(
+                'type' => 'string',
+                'default' => ''
+                ),
             'anchor' => array(
                 'type' => 'string',
                 'default' => ''
@@ -123,9 +138,9 @@ function artstation_gallery_register_block() {
                 'type' => 'string',
                 'default' => ''
                 ),
-			)
-		)
-	);
+            )
+        )
+    );
 
 }
 add_action( 'init', 'artstation_gallery_register_block' );
